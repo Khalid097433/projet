c9e7e58ad06b41c64d99ab4f0f7ff1afa718c7de
Khalid097433 <124538474+Khalid097433@users.noreply.github.com>
2023-12-23 19:37:21 +0300

    Update bibliotheque.java

--- src/bibliotheque.java
+++ src/bibliotheque.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
 public class bibliotheque {
-    ArrayList <livre> mes_livres ;
+    ArrayList <Livre> mes_livres ;
 
     public bibliotheque() {
-        mes_livres = new ArrayList<livre>();
+        mes_livres = new ArrayList<Livre>();
     }
 
-    public ArrayList<livre> getMes_livres() {
+    public ArrayList<Livre> getMes_livres() {
         return mes_livres;
     }
 
@@ -18,32 +18,39 @@ public class bibliotheque {
                 "mes_livres=" + mes_livres +
                 '}';
     }
-    public void ajouter_livre (livre l){
+    public void ajouter_livre (Livre l){
         if(!mes_livres.contains(l)){
-            if(l.getMon_bibliotheque()!=null){
-                l.getMon_bibliotheque().enleve(l);
+            if(l.getMaBibliotheque()!=null){
+                l.getMaBibliotheque().enleve(l);
             }
         }
         mes_livres.add(l);
-        l.setMon_bibliotheque(this);
+        l.setMaBibliotheque(this);
     }
-    public void enleve (livre l){
+    public void enleve (Livre l){
         if(mes_livres.contains(l)){
             mes_livres.remove(l);
-            l.setMon_bibliotheque(null);
+            l.setMaBibliotheque(null);
         }
 
     }
     public void afficher_catalogue() {
         for (int i=0 ; i<mes_livres.size();i++){
-            mes_livres.get(i).afficher_details();
+            mes_livres.get(i).afficherDetails();
         }
     }
-    public void emprunter_livre(String t) throws LivreIndisponibleException {
-        for (int i = 0; i < mes_livres.size(); i++) {
-            if (mes_livres.get(i).getTitre() == t) {
-                if (!mes_livres.get(i).isDisponible()) throw new LivreIndisponibleException();
+    public void emprunter_livre(String titre) throws LivreIndisponibleException {
+        for (Livre livre : mes_livres) {
+            if (livre.titre.equals(titre)) {
+                if (livre.disponible) {
+                    livre.disponible = false;
+                    System.out.println("Livre emprunté : " + titre);
+                    return;
+                } else {
+                    throw new LivreIndisponibleException("Livre indisponible pour emprunt.");
+                }
             }
         }
+        System.out.println("Livre non trouvé dans la bibliothèque.");
     }
 }
