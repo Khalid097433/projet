9152aac2d088e750b5000ba604fb1b4b7a873167
Khalid097433 <124538474+Khalid097433@users.noreply.github.com>
2023-12-23 19:36:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        livre l1 , l2;
-        l1 = new LivreNumerique("PDF" , "baris" , true , "bitch");
-        l2 = new LivrePapier (500,"la vie qu'on mene","houssein" , true);
+        Livre l1 , l2;
+        l1 = new LivreNumerique("PDF" , "baris" , "bitch" , true);
+        l2 = new LivrePapier (500,"la vie qu'on mene","houssein" , false);
         bibliotheque b1 = new bibliotheque();
         b1.ajouter_livre(l1);
         b1.ajouter_livre(l2);
         try {
+            b1.emprunter_livre("baris");
             b1.emprunter_livre("la vie qu'on mene");
         } catch (LivreIndisponibleException e) {
-            System.out.println(e);
+            System.out.println("Erreur de retrait : " + e.getMessage());
         }
     }
 }
